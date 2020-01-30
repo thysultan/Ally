1322625047faf3c54c5dcb6f31215ea6cf8b96a1
thysultan <sultantarimo@me.com>
2020-01-30 19:47:46 +0300

    draft of abstract syntax tree structure.

--- poc.js
+++ poc.js
@@ -1,3 +1,132 @@
+// interface: {type: 'string', props: string[], children: interface[], source: float}
+// program: {type: 'program', props: [], children: []}
+// definition: {type: 'definition', props: [], children: []}
+// identifier: {type: 'identifier', props: [], children: []}
+// literal: {type: 'literal', props: [], children: []}
+// statement: {type: 'statement', props: [], children: []}
+// expression: {type: 'expressin', props: [], children: []}
+// declaration: {type: 'declaration', props: [], children: []}
+// operator: {type: 'operator', props: [], children: []}
+
+/*
+int number = 10 000km
+
+{
+	type: 'declaration',
+	props: ['int', 'number'],
+	children: {
+		type: 'literal',
+		props: ['int', '10000'],
+		children: []
+	}
+}
+
+obj object = {int length = 0, str string: 'a'}
+
+{
+	type: 'declaration',
+	props: ['obj', 'object'],
+	children: {
+		type; 'literal',
+		props: ['obj', '2'],
+		children: [
+			{
+				type: 'declaration',
+				props: ['int', 'length'],
+				children: {
+					type: 'literal',
+					props: ['int', '0'],
+					children: []
+				}
+			},
+			{
+				type: 'declaration',
+				props: ['str', 'string'],
+				children: {
+					type: 'literal',
+					props: ['str', 'a'],
+					children: []
+				}
+			}
+		]
+	}
+}
+
+ptr rawptr = {1024}
+
+{
+	type: 'declaration',
+	props: ['ptr', 'rawptr'],
+	children: [
+		{
+			type: 'literal',
+			props: ['ptr', '1024'],
+			children: []
+		}
+	]
+}
+
+fun fib name, age {}
+
+{
+	type: 'declaration',
+	props: ['fun', 'fib'],
+	children: [
+		{
+			type: 'declaration',
+			props: ['name', 'age'],
+			children: [
+				{
+					type: 'identifier',
+					props: ['any', 'name'],
+					children: []
+				},
+				{
+					type: 'identifier',
+					props: ['any', 'age'],
+					children: []
+				}
+			]
+		},
+		{
+			type: 'statement',
+			props: [],
+			children: []
+		}
+	]
+}
+
+a = b = c // (a = (b = c))
+
+{
+	type: 'expression',
+	props: ['assignment'],
+	children: [
+		{
+			type: 'identifier',
+			props: ['any', 'a'],
+			children: []
+		},
+		{
+			type: 'expression',
+			props: ['equal'],
+			children: [
+				{
+					type: 'identifier',
+					props: ['any', 'b']
+					children: []
+				},
+				{
+					type: 'identifier',
+					props: ['any', 'c'],
+					children: []
+				}
+			]
+		}
+	]
+}
+*/
+
 export var line = 0
 export var column = 0
 export var length = 0
@@ -5,64 +134,68 @@ export var position = 0
 export var character = 0
 export var characters = ''
 
+export function allocate (value) {
+	characters = value, position = column = 0
+}
+
 /**
  * @param {string} value
  * @return {string[]}
  */
 export function compile (value) {
-	return parse(position = column = 0, characters = value)
+	return parse(position = column = 0, 0, [characters = value], [])
 }
 
 /**
  * @param {string} value
+ * @param {number} type
+ * @param {number} context
  * @return {object}
  */
-export function parse (type, value) {
+export function parse (context, type, props, children) {
 	var code = 0
 	var index = 0
 	var offset = 0
 	var length = 0
+	var position = 0
 	var scanning = 1
 
 	while (scanning) {
-		switch (scan()) {
-			// &
-			case 38:
+		switch (next()) {
+			// \0 ) ] }
+			case 0: case type: index = offset = length = position = scanning = 0
 				break
 			// ,
 			case 44:
 				break
-			// :
-			case 58:
-				break
 			// ;
 			case 59:
 				break
-			// [ {
-			case 91: case 123: move(1)
-			// ( )
-			case 40: parse(char() + 1, value)
+			// ! % & * + - / < = > ^ |
+			case 33; case 37: case 38: case 42: case 43: case 45: case 47: case 60: case 61: case 62: case 94: case 124:
 				break
-			// \0 ) ] }
-			case 0: case type: index = offset = length = scanning = 0
+			// [ {
+			case 91: case 123: shift(1)
+			// ( )
+			case 40: parse(context, char() + 1, props, children)
 				break
 			// " ' ` string
-			case 34: case 39: case 96: index = caret()
-				while (code = scan()) {
+			case 34: case 39: case 96: position = caret()
+				while (code = next()) {
 					if (code == 92) {
-						scan()
+						next()
 					} else if (code == char()) {
 						break
 					}
 				}
 
-				substr(index, caret())
+				substr(position, caret())
 				break
-			// \n \t \s whitespace
-			case 10: ++line, column = caret() case 9: case 32:
+			// \t \n \s whitespace
+			case 9: case 10: case 32: ++line, column = caret() case 9: case 32:
 				while (code = peek()) {
 					if (code < 32) {
-						scan()
+						next()
 					} else {
 						break
 					}
@@ -73,15 +206,15 @@ export function parse (type, value) {
 				switch (peek()) {
 					// *
 					case 42:
-						while (scan()) {
-							if (char() == 42 && scan() == 47) {
+						while (next()) {
+							if (char() == 42 && next() == 47) {
 								break
 							}
 						}
 						break
 					// /
 					case 47:
-						while (scan()) {
+						while (next()) {
 							if (char() == 10) {
 								break
 							}
@@ -90,21 +223,31 @@ export function parse (type, value) {
 				}
 				break
 			// 0-9 numbers
-			case 48: case 49: case 48: case 50: case 51: case 52: case 53: case 54: case 55: case 56: case 57:
+			case 48: case 49: case 48: case 50: case 51: case 52: case 53: case 54: case 55: case 56: case 57: position = caret()
+				while (code = next()) {
+					if (code < 32) {
+						whitespace()
+					} else if (code > 64) {
+						identifier()
+					}
+				}
+
+				substr(position, caret())
 				break
 			// _
-			case 95: offset = 1, scan()
+			case 95: offset = 1, next()
 			// A-z identifiers
-			default: index = caret()
+			default: position = caret()
 				while (code = peek()) {
 					if (identifier()) {
-						scan()
+						next()
 					} else {
 						break
 					}
 				}
 
-				switch (substr(index - offset, caret())) {
+				// keywords
+				switch (substr(position - offset, caret())) {
 					// types
 					case 'int':
 					case 'big':
@@ -114,25 +257,26 @@ export function parse (type, value) {
 					case 'ptr':
 					case 'nil':
 					case 'obj':
-					case 'def':
-					case 'fun':
 					case 'any':
 					case 'var':
+						break
+					case 'def':
+					case 'fun':
 						break
 					// control
 					case 'if':
 					case 'else':
 					case 'switch':
-					case 'break':
-					case 'continue':
 					case 'for':
-					case 'of':
 					case 'try':
 					case 'catch':
-					case 'in':
+					case 'break':
+					case 'continue':
 					case 'pick':
+					case 'of':
+					case 'in':
 					case 'as':
-					case 'import'
+					case 'import':
 					case 'export':
 					case 'sizeof':
 					case 'typeof':
@@ -152,18 +296,17 @@ export function peek (position) {
 }
 
 /**
- * @param {number} distance
  * @return {number}
  */
-export function move (distance) {
-	return character += distance
+export function next () {
+	return character = peek(position++)
 }
 
 /**
  * @return {number}
  */
-export function scan () {
-	return character = peek(position++)
+export function prev () {
+	return character = peek(position--)
 }
 
 /**
@@ -171,6 +314,14 @@ export function scan () {
  */
 export function char () {
 	return character
+}
+
+/**
+ * @param {number} distance
+ * @return {number}
+ */
+export function shift (distance) {
+	return character += distance
 }
 
 /**
