49e0ad40ba31ae755cc93959c34bd7486d724fe5
thysultan <sultantarimo@me.com>
2020-04-14 00:17:01 +0300

    get it to work
    we should probably parse the numbers manually, first WIP number parser, with coments.

--- poc.js
+++ poc.js
@@ -1,7 +1,8 @@
 // stack
 export var head = 0
-export var body = 0
 export var tail = 0
+export var flag = 0
+export var root = null
 
 // input
 export var index = 0
@@ -17,12 +18,14 @@ export var length = 0
 export var memory = []
 export var symbol = {}
 
+console.log(compile('"100" var a 1 23 b 111km 10'), memory, symbol)
+
 /**
  * @param {string} value
  * @return {object}
  */
 export function compile (value) {
-	return tokenize(index = 0, input = value, node(token.program, value), null)
+	return tokenize(index = head++, input = value, root = node(token.program, value), root)
 }
 
 /**
@@ -33,32 +36,32 @@ export function compile (value) {
  * @return {object}
  */
 export function tokenize (type, props, next, root) {
-	while (tail = head, head = scan()) {
-		switch (head) {
+	while (tail = head) {
+		switch (flag = scan()) {
 			// < <= <<=
 			// > >= >>=
 			// * *= **=
-			case 42 case 60: case 62:
-				switch (peek(0)) {
+			case 42: case 60: case 62:
+				switch (peek()) {
 					// ** / << / >> / **= / <<= / >>=
-					case head: push(next, next = node(token.operator, [hash(head, scan(), peek(0) == 61 ? scan() : 1), types.num]))
+					case flag: push(next, next = node(token.operator, [hash(flag, scan(), peek() == 61 ? scan() : 1), types.num]))
 						break
 					// *=/ <= / >=
-					case 61: push(next, next = node(token.operator, [hash(head, scan()), types.bool]))
+					case 61: push(next, next = node(token.operator, [hash(flag, scan()), types.bool]))
 						break
 					// < / > / *
-					default: push(next, next = node(token.operator, [head, types.bool]))
+					default: push(next, next = node(token.operator, [flag, types.bool]))
 				}
 				break
 			// ! != !==
 			// = == ===
 			case 33: case 61:
-				switch (peek(0)) {
+				switch (peek()) {
 					// != / == / !== / ===
-					case 61: push(next, next = node(token.operator, [hash(head, scan(), peek(0) == 61 ? scan() : 1), types.bool]))
+					case 61: push(next, next = node(token.operator, [hash(flag, scan(), peek() == 61 ? scan() : 1), types.bool]))
 						break
 					// ! / =
-					default: push(next, next = node(token.operator, [head, head == 33 ? types.bool : types.var]))
+					default: push(next, next = node(token.operator, [flag, flag == 33 ? types.bool : types.var]))
 				}
 				break
 			// & && &=
@@ -66,57 +69,59 @@ export function tokenize (type, props, next, root) {
 			// - -- -=
 			// | || |=
 			case 38: case 43: case 45: case 124:
-				switch (peek(0)) {
+				switch (peek()) {
 					// && / ++ / -- / &= / += / -= / |=
-					case head: case 61: push(next, next = node(token.operator, [hash(head, scan()), types.num]))
+					case flag: case 61: push(next, next = node(token.operator, [hash(flag, scan()), types.num]))
 						break
 					// & / + / - / |
-					default: push(next, next = node(token.operator, [head, types.num]))
+					default: push(next, next = node(token.operator, [flag, types.num]))
 				}
 				break
 			// % %=
 			// ^ ^=
 			case 37: case 94:
-				switch (peek(0)) {
+				switch (peek()) {
 					// %= / ^=
-					case 61: push(next, next = node(token.operator, [hash(head, scan()), types.num]))
+					case 61: push(next, next = node(token.operator, [hash(flag, scan()), types.num]))
 						break
 					// % / ^
-					default: push(next, next = node(token.operator, [head, types.num]))
+					default: push(next, next = node(token.operator, [flag, types.num]))
 				}
 				break
 			// / /=
 			// // /*
 			case 47:
-				switch (peek(0)) {
-					// /* //
-					case 42: case 47: comment()
+				switch (peek()) {
+					// /*
+					case 42: flag = 42
+					// //
+					case 47: comment()
 						break
 					// /=
-					case 61: push(next, next = node(token.operator, [hash(head, scan()), types.num]))
+					case 61: push(next, next = node(token.operator, [hash(flag, scan()), types.num]))
 						break
 					// /
-					default: push(next, next = node(token.operator, [head, types.num]))
+					default: push(next, next = node(token.operator, [flag, types.num]))
 				}
 				break
 			// ? ?. ??
 			case 63:
-				switch (peek(0)) {
+				switch (peek()) {
 					// ?.  ??
-					case 46: case 63: push(next, next = node(token.operator, [hash(head, scan()), types.var]))
+					case 46: case 63: push(next, next = node(token.operator, [hash(flag, scan()), types.var]))
 						break
 					// ?
-					default: push(next, next = node(token.operator, [head, types.bool]))
+					default: push(next, next = node(token.operator, [flag, types.bool]))
 				}
 				break
 			// . .. ...
 			case 46:
-				switch (peek(0)) {
+				switch (peek()) {
 					// .. / ...
-					case 46: push(next, next = node(token.operator, [hash(head, scan(), peek(0) == 46 ? scan() : 1), types.ptr]))
+					case 46: push(next, next = node(token.operator, [hash(flag, scan(), peek() == 46 ? scan() : 1), types.ptr]))
 						break
 					// .
-					default: push(next, next = node(token.operator, [head, types.var]))
+					default: push(next, next = node(token.operator, [flag, types.var]))
 				}
 				break
 			// , ;
@@ -125,24 +130,30 @@ export function tokenize (type, props, next, root) {
 			// ) ] }
 			case type:
 				return root
-			// [ { (
-			case 91: case 123: ++body case 40: push(tail == 40 && head == 32 ? push(next, next = node(token.separator, props)) : next, parse(head = ++body, props, next = node(head, props), next)).props = next.next
+			// [ {
+			case 91: case 123: ++head
+			// (
+			case 40: push(tail == 40 && flag == 32 ? push(next, next = node(token.separator, props)) : next, parse(flag = ++head, props, next = node(flag, props), next)).props = next.next
 				break
 			// " '
 			case 34: case 39: push(next, next = node(token.literal, [alloc(slice(index, string())), types.str]))
 				break
-			// \t \n \s
-			case 9: case 10: head = 32 case 32: whitespace()
+			// \t \n
+			case 9: case 10: flag = 32
+			// \s
+			case 32: whitespace()
 				break
 			// 0-9 / A-z / _
 			default:
-				switch (alphanumeric(head)) {
-					case 1: push(next, next = node(token.literal, [digit(slice(index, number())), head < 0 ? types.flt : types.int]))
+				switch (alphanumeric(flag)) {
+					case 1: push(next, next = node(token.literal, [digit(slice(index - 1, number())), flag < 0 ? types.flt : types.int]))
 						break
-					case 2: push(next, next = node(tail = keyword(next = slice(index, identifier())), [tail == token.identifier ? table(next) : hash(head, body, -1):, types.var]))
+					case 2: push(next, next = node(tail = keyword(next = slice(index - 1, identifier())), [tail == token.identifier ? table(next) : hash(flag, head, -1), types.var]))
 				}
 		}
 	}
+
+	return root
 }
 
 /*
@@ -151,7 +162,7 @@ export function tokenize (type, props, next, root) {
  * @return {object}
  */
 export function node (type, props) {
-	return {type: type, props: props, children: null, next: null, position: position}
+	return {type: type, props: props, children: null, next: null, index: index}
 }
 
 /*
@@ -177,15 +188,22 @@ export function hash (head, body, tail) {
  * @return {number}
  */
 export function scan () {
-	return body = peek(index++)
+	return head = code(index++)
+}
+
+/**
+ * @return {number}
+ */
+export function peek () {
+	return code(index)
 }
 
 /**
  * @param {number} offset
  * @return {number}
  */
-export function peek (offset) {
-	return input.charCodeAt(index + offset)
+export function code (offset) {
+	return input.charCodeAt(offset) | 0
 }
 
 /**
@@ -202,7 +220,7 @@ export function slice (offset, length) {
  * @return {number}
  */
 export function digit (value) {
-	return Number(value)
+	return Number(value.replace(/[a-zA-Z\s]/g, ''))
 }
 
 /**
@@ -218,74 +236,72 @@ export function table (value) {
  * @return {number}
  */
 export function alloc (value) {
-	return memory[offset++] = value, offset
+	return memory[offset] = value, ++offset
 }
 
 /**
  * @return {number}
  */
 export function number () {
-	while (scan()) {
-		switch (peek(0)) {
-			// .
+	while (head) {
+		// record offset when encounting a dot .
+		// when done, add the second segment(if a float) a + (b / (10 ^ index - offset))
+		// add sign from tail if tail is -
+		switch (scan()) {
 			case 46:
-				// ..
-				if (head > 0) {
-					head = -1
+				if (flag > 0) {
+					flag = -index
 				} else {
-					return --index
+					return index -= 2
 				}
-			// \s
-			 case 32:
-				break
+		 	case 95:
+		 		break
+	 	 	case 32:
+	 	 		if (alphanumeric(peek()) == 1) {
+	 	 			head = 48
+	 	 		}
 			default:
-				switch (alphanumeric(peek(0))) {
-					case 0: return index
+				switch (alphanumeric(head)) {
+					case 0: return --index
+					case 1: // value = value * 10 + head - 48 // number
+						break
 					case 2: return identifier()
 				}
 		}
 	}
 
-	return index
+	return --index
 }
 
 /**
  * @return {number}
  */
 export function string () {
-	while (scan()) {
-		switch (peek(0)) {
-			// " '
-			case head:
-				return index
-			// \
-			case 92: scan()
+	while (head) {
+		if (scan() == flag) {
+			break
+		} else if (head == 92) {
+			scan()
 		}
 	}
 
-	return index
+	return index - 1
 }
 
 /**
  * @return {number}
  */
 export function comment () {
-	switch (peek(0)) {
-		// /
-		case 47:
-			while (scan()) {
-				if (body == 10) {
-					return index
-				}
+	while (head) {
+		if (flag == 47) {
+			if (scan() == 10) {
+				break
 			}
-			break
-		// *
-		case 42:
-			while (scan()) {
-				if (body == 42 && scan() == 47) {
-					return index
-				}
+		} else {
+			if (scan() == 42 && scan() == 47) {
+				break
 			}
+		}
 	}
 
 	return index
@@ -295,30 +311,26 @@ export function comment () {
  * @return {number}
  */
 export function whitespace () {
-	while (peek(0)) {
-		if (peek(0) < 32) {
-			scan()
-		} else {
+	while (head) {
+		if (scan() > 32) {
 			break
 		}
 	}
 
-	return index
+	return --index
 }
 
 /**
  * @return {number}
  */
 export function identifier () {
-	while (peek(0)) {
-		if (alphabetic(body)) {
-			scan()
-		} else {
+	while (head) {
+		if (!alphabetic(scan())) {
 			break
 		}
 	}
 
-	return index
+	return --index
 }
 
 /**
@@ -326,11 +338,11 @@ export function identifier () {
  * @return {number}
  */
 export function alphabetic (value) {
-	if (value == 94) {
+	if (value == 95) {
 		return 1
 	} else if (value > 64 && value < 91) {
 		return 2
-	} else if (value > 97 && value < 123) {
+	} else if (value > 96 && value < 123) {
 		return 3
 	} else {
 		return 0
@@ -342,7 +354,7 @@ export function alphabetic (value) {
  * @return {number}
  */
 export function alphanumeric (value) {
-	if (value > 48 && value < 58) {
+	if (value > 47 && value < 58) {
 		return 1
 	} else if (alphabetic(value)) {
 		return 2
